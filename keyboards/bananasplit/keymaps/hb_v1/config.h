447d6326db37c32b937858fd14a0902aaf4032b6
pimpelkram <hb@hb.gl>
2018-04-02 02:17:14 +0200

    bananasplit vollständig konfiguriert

--- keyboards/bananasplit/keymaps/hb_v1/config.h
+++ keyboards/bananasplit/keymaps/hb_v1/config.h
@@ -19,6 +19,13 @@
 
 #include "../../config.h"
 
+
+#define LSPO_KEY KC_8
+#define RSPC_KEY KC_9
+#define DISABLE_SPACE_CADET_ROLLOVER
+/*for Tap Dance: */
+//#define TAPPING_TERM 160
+
 // place overrides here
 
 #endif
--- keyboards/bananasplit/keymaps/hb_v1/keymap.c
+++ keyboards/bananasplit/keymaps/hb_v1/keymap.c
@@ -1,25 +1,173 @@
 #include "bananasplit.h"
 
-#define DEFAULT_LAYER 0
-#define LAYER_1       1
+#include "../../../quantum/keymap_extras/keymap_german.h"
+
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define _______ KC_TRNS
+
+#define KM_QWERTY  0
+#define LAYER1     1
+#define LAYER2     2
+#define MISC1      3
+#define MISC2      4
+
+//Tap Dance Declarations
+enum {
+  TD_COMM_EQL = 0,
+  TD_CAPS = 1,
+  TD_END = 2
+};
+
+
+
+
+/*capslock tap dance */
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_HOLD = 4, 
+  TRIPLE_HOLD = 6,
+  QUAD_HOLD = 8
+};
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || state->pressed==0) return SINGLE_TAP;
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    if (state->pressed) return DOUBLE_HOLD;
+    else return SINGLE_TAP;
+  } 
+  else if (state->count == 3) {
+    if (state->pressed) return TRIPLE_HOLD;
+    else return SINGLE_TAP;
+  }
+  else if (state->count == 4) {
+    if (state->pressed) return QUAD_HOLD;
+    else return SINGLE_TAP;
+  }
+  else return 10; //magic number. At some point this method will expand to work for more presses
+}
+
+/* instanz */
+static tap xtap_state = { 
+  .is_press_action = true,
+  .state = 0
+};
+
+
+void dance_capslock_finished (qk_tap_dance_state_t *state, void *user_data) {
+xtap_state.state = cur_dance(state);
+  switch (xtap_state.state) {
+    case SINGLE_TAP: register_code(KC_ESC); break;
+    case SINGLE_HOLD: layer_on(LAYER1); break;
+    case DOUBLE_HOLD: layer_on(LAYER1); register_code(KC_LSFT); break;
+    case TRIPLE_HOLD: layer_on(LAYER1); register_code(KC_LCTL); break;
+    case QUAD_HOLD:   layer_on(LAYER1); register_code(KC_LSFT); register_code(KC_LCTL);
+  }
+}
+
+void dance_capslock_reset (qk_tap_dance_state_t *state, void *user_data) {
+switch (xtap_state.state) {
+    case SINGLE_TAP: unregister_code(KC_ESC); break;
+    case SINGLE_HOLD: layer_off(LAYER1); break;
+    case DOUBLE_HOLD: layer_off(LAYER1); unregister_code(KC_LSFT); break;
+    case TRIPLE_HOLD: layer_off(LAYER1); unregister_code(KC_LCTL); break;
+    case QUAD_HOLD: layer_off(LAYER1); unregister_code(KC_LSFT); unregister_code(KC_LCTL);
+  }
+  xtap_state.state = 0;
+}
+
+
+
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_COMM_EQL]  = ACTION_TAP_DANCE_DOUBLE(KC_COMM, DE_EQL),
+  [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_capslock_finished, dance_capslock_reset),
+  [TD_END] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_END)
+// Other declarations would go here, separated by commas, if you have them
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [DEFAULT_LAYER] = KEYMAP(
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, \
-    KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT, KC_ENT, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),  KC_UP, _______, \
-    KC_LCTL,  KC_LGUI,  KC_LALT, KC_ENT,      MO(LAYER_1),      KC_SPC,  KC_RALT, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
 
-  [LAYER_1] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______,          _______, _______, _______,          _______, _______, _______, _______, _______ \
-  ),
+  [KM_QWERTY] = KEYMAP_ALL(
+    F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_NO,   //15 slots
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, CTL_T(KC_ENT),  // 14 slots
+    TD(TD_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(LAYER2,KC_SCLN), TD(TD_END),KC_BSLS , //13 slots
+    KC_LSPO, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_NO, //14 slots
+    KC_LCTL, KC_LGUI, KC_LALT, LT(MISC1, KC_INS), LT(MISC2,KC_DEL), KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT), //11 slots
+
+  [MISC1] = KEYMAP_ALL(
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL, 
+    KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, RESET, RESET, RESET, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+  [MISC2] = KEYMAP_ALL(
+    KC_TRNS, M(1), M(2), M(3), M(4), M(5), M(6), M(7), M(8), M(9), M(10), M(11), M(12), KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+  [LAYER1] = KEYMAP_ALL( \
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______,_______, \
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   \
+    _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP, KC_RGHT ,KC_BSPC,DE_EQL ,        _______,   \
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_UP,_______,   \
+    _______,_______,_______,_______,_______,_______,_______,                        _______,KC_LEFT,KC_DOWN  ,KC_RGHT  ),
+
+  /* Layer 2: Homerow ArrowKeys */
+  [LAYER2] = KEYMAP_ALL( \
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______,_______, \
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_INS ,_______,_______,_______,_______,   \
+    _______,DE_LESS,DE_MORE,DE_LCBR,DE_RCBR,DE_LBRC,DE_RBRC,KC_HOME,KC_END,KC_DEL  ,_______,_______,        _______,   \
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   \
+    _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______  ,_______  ),
+  
+  
+};
+
+enum function_id {
+    SHIFT_ESC,
 };
 
 const uint16_t PROGMEM fn_actions[] = {
+  [0]  = ACTION_FUNCTION(SHIFT_ESC),
 };
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  static uint8_t shift_esc_shift_mask;
+  switch (id) {
+    case SHIFT_ESC:
+      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
+      if (record->event.pressed) {
+        if (shift_esc_shift_mask) {
+          add_key(KC_GRV);
+          send_keyboard_report();
+        } else {
+          add_key(KC_ESC);
+          send_keyboard_report();
+        }
+      } else {
+        if (shift_esc_shift_mask) {
+          del_key(KC_GRV);
+          send_keyboard_report();
+        } else {
+          del_key(KC_ESC);
+          send_keyboard_report();
+        }
+      }
+      break;
+  }
+}
