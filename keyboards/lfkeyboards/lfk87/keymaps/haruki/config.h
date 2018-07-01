2d63a6dbe86f6b26b73550c65d7ebdcf8e0817b2
Pimpelkram <hb@hb.gl>
2018-07-01 23:55:58 +0200

    lfk87 new layer (left space)

--- keyboards/lfkeyboards/lfk87/keymaps/haruki/config.h
+++ keyboards/lfkeyboards/lfk87/keymaps/haruki/config.h
@@ -1,5 +1,6 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
+#define PERMISSIVE_HOLD
 
 #include "../../config.h"
 
@@ -7,5 +8,9 @@
 #define LSPO_KEY KC_8
 #define RSPC_KEY KC_9
 #define DISABLE_SPACE_CADET_ROLLOVER
+
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(IMPERIAL_MARCH)
+#endif
 
 #endif
