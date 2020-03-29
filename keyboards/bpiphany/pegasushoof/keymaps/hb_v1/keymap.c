7b00ba8fc6945a81dc801be48e16f7fe2271d00a
pimpelkram <hb@hb.gl>
2020-03-29 18:54:27 +0200

    updated pegasus_hoof

--- keyboards/bpiphany/pegasushoof/keymaps/hb_v1/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/hb_v1/keymap.c
@@ -15,7 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "pegasushoof.h"
+#include QMK_KEYBOARD_H
+/*#include "pegasushoof.h"*/
 #include "action_layer.h"
 #include "../../../../../quantum/keymap_extras/keymap_german.h"
 
