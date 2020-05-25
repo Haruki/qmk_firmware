bdf7f3f3ac54ba7a82396a58bb38a1256fc06f17
pimpelkram <hb@hb.gl>
2020-05-25 18:45:35 +0200

    dz60: added gaming toggle and F_ROW layer

--- keyboards/dz60/keymaps/hb_v1/keymap.c
+++ keyboards/dz60/keymaps/hb_v1/keymap.c
@@ -10,6 +10,8 @@
 #define LAYER2     2
 #define MISC1      3
 #define MISC2      4
+#define GAMING     5
+#define F_ROW	   6
 
 //Tap Dance Declarations
 enum {
@@ -102,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[KM_QWERTY] = LAYOUT(
 		F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
-		LT(LAYER1,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(LAYER2,KC_SCLN), TD(TD_END), CTL_T(KC_ENT), 
+		LT(LAYER1,KC_ESC), LT(F_ROW,KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(LAYER2,KC_SCLN), TD(TD_END), CTL_T(KC_ENT), 
 		KC_LSPO, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_NO, 
 		KC_LCTL, KC_LGUI, KC_LALT, LT(MISC1, KC_INS), LT(MISC2,KC_DEL), KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
@@ -123,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER1] = LAYOUT( \
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______,_______, \
     _______,LSFT(KC_1),LSFT(KC_2),LSFT(KC_3),LSFT(KC_4),LSFT(KC_5),LSFT(KC_6),LSFT(KC_7),LSFT(KC_8),LSFT(KC_9),LSFT(KC_0), LSFT(KC_MINS), LSFT(KC_EQL), LSFT(KC_NO),   \
-    _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP, KC_RGHT ,KC_BSPC,DE_EQL ,        _______,   \
+    _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP, KC_RGHT ,KC_BSPC,DE_EQL ,        TG(GAMING),   \
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_UP,_______,   \
     _______,_______,_______,_______,_______,_______,_______,                        _______,KC_LEFT,KC_DOWN  ,KC_RGHT  ),
 
@@ -135,7 +137,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,_______,_______,_______,_______,_______,DE_GRV,DE_QUOT,_______,_______,_______,_______,_______,   \
     _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______  ,_______  ),
   
-  
+	[GAMING] = LAYOUT(
+		F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
+		_______, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, TD_END, CTL_T(KC_ENT), 
+		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_NO, 
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+
+ 	[F_ROW] = LAYOUT(
+		F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_F7, KC_F8, KC_F9, KC_F10, KC_LBRC, KC_RBRC, KC_BSLS, 
+		_______, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_F4, KC_F5, KC_F6, KC_F11, TD_END, CTL_T(KC_ENT), 
+		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_F1, KC_F2, KC_F3, KC_F12, KC_RSFT, KC_NO, 
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+
 };
 
 enum function_id {
