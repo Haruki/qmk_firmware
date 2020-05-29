96c89c5a38c1e7c86563fc33074b5ee3ec1892d6
pimpelkram <hb@hb.gl>
2020-05-29 16:34:58 +0200

    pegasushoof: added gaming layer standard

--- keyboards/bpiphany/pegasushoof/keymaps/hb_v1/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/hb_v1/keymap.c
@@ -29,10 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _______ KC_TRNS
 
 #define KM_QWERTY  0
-#define LAYER1     1
-#define LAYER2     2
-#define LAYER3	   3
-#define GAMING	   4
+#define GAMING     1
+#define LAYER1     2
+#define LAYER2     3
+#define LAYER3	   4
 #define MISC       5
 
 //Tap Dance Declarations
@@ -129,14 +129,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,  KC_DEL, KC_END, KC_PGDN, \
 		LT(LAYER1,KC_ESC), LT(LAYER3,KC_A),   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   LT(LAYER2,KC_SCLN),TD(TD_END),CTL_T(KC_ENT),              \
 		KC_LSPO, KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM,KC_DOT, KC_SLSH,        KC_RSPC,          KC_UP,              \
-		KC_LCTL, KC_LGUI,   KC_LALT,                       KC_SPC,                          KC_RALT,KC_RGUI,TG(GAMING),KC_FN0,   KC_LEFT,KC_DOWN,KC_RGHT),	
+		KC_LCTL, KC_LGUI,   KC_LALT,                       KC_SPC,                          KC_RALT,KC_RGUI,KC_NO,KC_FN0,   KC_LEFT,KC_DOWN,KC_RGHT),	
+
+	[GAMING] = LAYOUT( \
+		KC_ESC,             KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,   KC_PSCR,KC_SLCK,KC_PAUS, \
+		KC_GRV,  KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,  KC_INS, KC_HOME,KC_PGUP, \
+		KC_TAB,  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,  KC_DEL, KC_END, KC_PGDN, \
+		_______, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,TD_END,CTL_T(KC_ENT),              \
+		KC_LSPO, KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM,KC_DOT, KC_SLSH,        KC_RSPC,          KC_UP,              \
+		KC_LCTL, KC_LGUI,   KC_LALT,                       KC_SPC,                          KC_RALT,KC_RGUI,KC_NO,KC_FN0,   KC_LEFT,KC_DOWN,KC_RGHT),	
 
 	/* Layer 1: Homerow ArrowKeys */
 	[LAYER1] = LAYOUT( \
 		_______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,_______,_______, \
 		_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,_______,   _______,_______,_______, \
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,_______,_______, \
-		_______,KC_PSCR,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP, KC_RGHT ,KC_BSPC,DE_EQL ,        _______,                            \
+		_______,KC_PSCR,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP, KC_RGHT ,KC_BSPC,DE_EQL ,        TG(GAMING),                         \
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,           _______,         \
 		_______,_______,_______,                        KC_DEL,                        _______,_______,_______  ,_______,   _______,_______,_______),
 
@@ -156,14 +164,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,_______,_______,_______,_______,_______,_______,KC_4, KC_5, KC_6,_______,_______,        _______,                            \
 		_______,_______,_______,_______,_______,_______,_______,_______,KC_1, KC_2, KC_3,_______,        _______,           _______,         \
 		_______,_______,_______,                        _______,                        _______,_______,_______  ,_______,   _______,_______,_______),
-
-	[GAMING] = LAYOUT( \
-		KC_ESC,             KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,   KC_PSCR,KC_SLCK,KC_PAUS, \
-		KC_GRV,  KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,  KC_INS, KC_HOME,KC_PGUP, \
-		KC_TAB,  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,  KC_DEL, KC_END, KC_PGDN, \
-		KC_CAPSLOCK, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,              \
-		KC_LSPO, KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM,KC_DOT, KC_SLSH,        KC_RSPC,          KC_UP,              \
-		KC_LCTL, KC_LGUI,   KC_LALT,                       KC_SPC,                          KC_RALT,KC_RGUI,_______,_______,   KC_LEFT,KC_DOWN,KC_RGHT),
 
 	/* Misc Layer: Function layer */
 	[MISC] = LAYOUT( \
